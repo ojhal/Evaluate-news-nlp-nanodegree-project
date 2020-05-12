654c27701cd4e428a7a163529c96ab8e0b4f00da
Ojhal Patwa <patkarojhal26@gmail.com>
2020-05-12 13:02:16 +0530

    Add fuctionality

--- src/client/index.js
+++ src/client/index.js
@@ -1,16 +1,11 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
-console.log(checkForName);
-
-alert("I EXIST")
-console.log("CHANGE!!");
-
-import './styles/base.scss'
-import './styles/footer.scss'
-import './styles/form.scss'
-import './styles/header.scss'
-import './styles/resets.scss'
+// import './styles/resets.css'
+// import './styles/base.css'
+// import './styles/footer.css'
+// import './styles/form.css'
+// import './styles/header.css'
 
 export{
     checkForName,
--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,27 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+var aylien = require("aylien_textapi");
+const bodyParser = require('body-Parser');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+const textapi = new aylien({
+    application_id:  `${process.env.API_ID}`,
+    application_key:  `${process.env.API_KEY}`
+  });
+
 
 const app = express()
+
+
+
+/* Middleware*/
+
+app.use(bodyParser.urlencoded({extended: fasle}));
+app.use(bodyParser.json());
 
 app.use(express.static('dist'))
 
@@ -10,7 +29,7 @@ console.log(__dirname)
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile(path.resolve('dist/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
